9fd54751951c2c6dd446ec0ead1e983c9fd2b5d3
DrLag20 <vladimirjosedisla@gmail.com>
2024-09-11 17:57:38 -0400

    Modern_dict.py

--- Modern_dict.py
+++ Modern_dict.py
@@ -2,6 +2,8 @@ modern_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso.",
             "LOL": "Una respuesta común a algo gracioso.",
             "ROFL": "ROFL se utiliza como reacción a algo gracioso, similar a LOL.",
+            "AGGRO": "Ponerse agresivo o enojado.",
+            "KLK": "Forma de saludo, como decir: ¿Qué pasa?",
             "CREEPY": "Aterrador, siniestro.",
 
             } 
